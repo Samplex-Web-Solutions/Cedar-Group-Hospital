0410fc843253af30b92681539dddb824ea2029c4
Samplex-Web-Solutions <samplexwebsolutions@gmail.com>
2026-05-21 10:36:24 +0100

    form fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   const requiredFields = [
     "fullName", "maritalStatus", "dob", "tel1", "gender",
     "currentAddress", "permanentAddress", "nationality",
-    "stateOfOrigin", "occupation", "gender",
+    "stateOfOrigin", "gender",
     "emergencyName", "emergencyPhone", "nextOfKin","emergencyAddress",
   ];
 
@@ -55,7 +55,7 @@ function App() {
     });
 
     toast.dismiss(loadingToast);
-    toast.success("Registration Successful!");
+    toast.success("Form Submitted Successful!");
     setStep(1); 
   } catch (error) {
     toast.dismiss(loadingToast);
@@ -81,13 +81,13 @@ function App() {
         <AnimatePresence mode="wait">
           {step === 1 && (
             <FormStep key="s1">
-              <FormField label="Full Name" name="fullName" value={formData.fullName} onChange={handleChange} />
+              <FormField label="Full Name *" name="fullName" value={formData.fullName} onChange={handleChange} />
               <div className="grid gap-4">
-                <FormField label="Date of Birth" name="dob" type="date" value={formData.dob} onChange={handleChange} />
-                <FormField label="Gender" name="gender" required as="select" options={["Male", "Female", "Other"]} value={formData.gender} onChange={handleChange} />
+                <FormField label="Date of Birth *" name="dob" type="date" value={formData.dob} onChange={handleChange} />
+                <FormField label="Gender *" name="gender" required as="select" options={["Male", "Female", "Other"]} value={formData.gender} onChange={handleChange} />
               </div>
-              <FormField label="Phone 1" name="tel1" value={formData.tel1} onChange={handleChange} />
-              <FormField label="Phone 2" name="tel2" value={formData.tel2} onChange={handleChange} />
+              <FormField label="Phone 1 *" type="tel" name="tel1" value={formData.tel1} onChange={handleChange} />
+              <FormField label="Phone 2" type="tel" name="tel2" value={formData.tel2} onChange={handleChange} />
               <FormField label="Email" name="email" value={formData.email} onChange={handleChange} />
               <button type="button" onClick={() => setStep(2)} className="w-full bg-purple-900 hover:bg-purple-700 duration-400 text-white py-3 rounded-xl font-bold mt-4">Next</button>
             </FormStep>
@@ -95,11 +95,11 @@ function App() {
 
           {step === 2 && (
             <FormStep key="s2">
-              <FormField label="Current Address" name="currentAddress" as="textarea" value={formData.currentAddress} onChange={handleChange} />
-              <FormField label="Permanent Address"  name="permanentAddress" as="textarea" value={formData.permanentAddress} onChange={handleChange} />
+              <FormField label="Current Address *" name="currentAddress" as="textarea" value={formData.currentAddress} onChange={handleChange} />
+              <FormField label="Permanent Address *"  name="permanentAddress" as="textarea" value={formData.permanentAddress} onChange={handleChange} />
               <div className="grid grid-cols-2 gap-4">
-                <FormField label="Nationality" name="nationality"  value={formData.nationality} onChange={handleChange} />
-                <FormField label="State of Origin" name="stateOfOrigin" required value={formData.stateOfOrigin} onChange={handleChange} />
+                <FormField label="Nationality *" name="nationality"  value={formData.nationality} onChange={handleChange} />
+                <FormField label="State of Origin *" name="stateOfOrigin" required value={formData.stateOfOrigin} onChange={handleChange} />
               </div>
               <FormField label="Occupation" name="occupation" value={formData.occupation} onChange={handleChange} />
               <div className="flex gap-4 mt-6">
@@ -113,13 +113,13 @@ function App() {
             <FormStep key="s3">
               <FormField label="Allergies" name="allergies" as="text" value={formData.allergies} onChange={handleChange} />
               <div className="grid gap-4">
-                <FormField label="Marital Status" name="maritalStatus" as="select" options={["Single", "Married", "Divorced", "Widowed"]} value={formData.maritalStatus} onChange={handleChange} />
+                <FormField label="Marital Status *" name="maritalStatus" as="select" options={["Single", "Married", "Divorced", "Widowed"]} value={formData.maritalStatus} onChange={handleChange} />
               </div>
               <div className="grid gap-4">
                 <FormField label="Religion" name="religion" value={formData.religion} onChange={handleChange} />
               </div>
 
-              <FormField label="Next of Kin" name="nextOfKin" value={formData.nextOfKin} onChange={handleChange} />
+              <FormField label="Next of Kin *" name="nextOfKin" value={formData.nextOfKin} onChange={handleChange} />
               <FormField label="How did you hear about us? (Referral)" name="referral" as="select" options={["Friend/Family", "Social Media", "Google Search", "Doctor Referral", "Insurance Provider", "Emergency Visit", "Previous Patient", "Other"]} value={formData.referral} onChange={handleChange} />
               <div className="flex gap-4 mt-6">
                 <button type="button" onClick={() => setStep(2)} className="flex-1 bg-white/50 hover:bg-white/20 duration-400 text-white py-3 rounded-xl">Back</button>
@@ -130,9 +130,9 @@ function App() {
 
           {step === 4 && (
             <FormStep key="s4">
-              <FormField label="Emergency Name" name="emergencyName" value={formData.emergencyName} onChange={handleChange} />
-              <FormField label="Emergency Contact" name="emergencyPhone" value={formData.emergencyPhone} onChange={handleChange} />
-              <FormField label="Emergency Address" name="emergencyAddress" value={formData.emergencyAddress} onChange={handleChange} />
+              <FormField label="Emergency Contact Name *" name="emergencyName" value={formData.emergencyName} onChange={handleChange} />
+              <FormField label="Emergency Contact Phone *" name="emergencyPhone" value={formData.emergencyPhone} onChange={handleChange} />
+              <FormField label="Emergency Contact Address *" name="emergencyAddress" value={formData.emergencyAddress} onChange={handleChange} />
               <FormField label="Insurance Name" name="insuranceName" value={formData.insuranceName} onChange={handleChange} />
               <FormField label="Insurance ID" name="insuranceId" value={formData.insuranceId} onChange={handleChange} />
               <div className="flex gap-4 mt-6">
